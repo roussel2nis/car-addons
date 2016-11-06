0ef606d5a8e322313e146a1841cf0a680605fd22
rousseldenis <roussel2nis@hotmail.com>
2016-11-06 22:06:03 +0100

    [ADD] All dependencies

---
+++ car_all/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Denis Roussel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Car All',
+    'description': """
+        All Dependencies""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Denis Roussel',
+    'depends': ['car_pos_loyalty'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': False
+}
