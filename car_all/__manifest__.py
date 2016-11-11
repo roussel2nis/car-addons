2f3888f5fc7438f6986a6f7dd39be0968d81dbd7
rousseldenis <roussel2nis@hotmail.com>
2016-11-11 08:55:00 +0100

    [ADD] Purchase

--- car_all/__manifest__.py
+++ car_all/__manifest__.py
@@ -9,7 +9,12 @@
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Denis Roussel',
-    'depends': ['car_pos_loyalty'
+    'depends': ['sale',
+                'purchase',
+                'point_of_sale',
+                'account',
+                'account_accountant',
+                'car_pos_loyalty'
     ],
     'data': [
     ],
